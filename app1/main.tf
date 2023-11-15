b8efa600f2f190ed139a0ddc3c8f3d26f168c1f3
peter <peterkwende@yahoo.com>
2023-11-14 20:26:37 -0500

    initial commit

---
+++ app1/main.tf
@@ -0,0 +1,12 @@
+module "sever1" {
+  source = "../modules/ec2"
+  ami = "ami-0230bd60aa48260c6"
+  region = "us-east-1"
+  instance_type = "t3.small" 
+  name = "DEV-from module"
+    
+    
+  }
+  output "public-ip" {
+    value = module.sever1.public-ip
+  }
